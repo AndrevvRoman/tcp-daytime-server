b332785de6902a737882c359d49942395c452fee
Andrey Romanov <andrey_romanof@mail.ru>
2021-10-27 16:22:11 +0700

    Добавил реализацию сущности, которая отвечает на запросы

--- TimeManager.cpp
+++ TimeManager.cpp
@@ -4,4 +4,13 @@ void TimeManager::start(std::shared_ptr<IGetTimeEndpoint> getTimeEndpoint,std::s
 {
     m_getTimeEndpoint = getTimeEndpoint;
     m_networkEndpoint = networkEndpoint;
+
+    m_networkEndpoint->subscribeToRead([this](const uint16_t id,const std::string & message)
+    {
+        auto timeShiftResult = m_getTimeEndpoint->getNowInTimezone(message);
+        if (timeShiftResult.second)
+            m_networkEndpoint->scheduleWrite(id,timeShiftResult.first);
+        else
+            m_networkEndpoint->scheduleWrite(id,"I don't know inputed timezone");
+    });
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <memory>
+#include <thread>
+
+#include "TimeManager.hpp"
+#include "IGetTimeEndpoint.hpp"
+#include "INetworkEndpoint.hpp"
+#include "NetworkEndpoint.hpp"
+#include "ChronoTimeEndpoing.hpp"
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+    std::shared_ptr<INetworkEndpoint> network = std::make_shared<NetworkEndpoint>(8000);
+    std::shared_ptr<IGetTimeEndpoint> clock = std::make_shared<ChronoTimeEndpoint>();
+    TimeManager timeManager;
+    timeManager.start(clock,network);
+
+    network->start();
+    while(true)
+    {
+        std::cout << "I'm alive!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+    }
 }
